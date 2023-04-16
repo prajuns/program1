256e74e7cd486753b90bd81bf9371a92f5648eeb
prajuns <130234119+prajuns@users.noreply.github.com>
2023-04-16 12:34:23 +0530

    delete task directory

--- task/taskapp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-# Create your models here.
-class Bird(models.Model):
-    name=models.CharField(max_length=250)
-    img=models.ImageField(upload_to='photos')
-    desc=models.TextField()
